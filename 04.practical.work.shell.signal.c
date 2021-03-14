69e70eef8858c108bbcf4487f67cefe0b9cb9538
Hieu <goldenrezgun@gmail.com>
2021-03-14 09:31:07 +0700

    04.practical.work.shell.signal.c

---
+++ 04.practical.work.shell.signal.c
@@ -0,0 +1,31 @@
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void handler(int signal_num) {
+printf("Signal %d => ", signal_num);
+switch (signal_num) {
+case SIGTSTP:
+printf("pause\n"); break;
+case SIGINT:
+case SIGTERM:
+printf("Terminated\n");
+exit(0);
+break;
+}
+}
+int main(void) {
+// ctrl z
+signal(SIGTSTP, handler);
+// ctrl c or killed
+signal(SIGINT, handler);
+signal(SIGTERM, handler);
+while (1) {
+sleep(1);
+printf(".\n");
+}
+printf("end");
+return 0;
+}
