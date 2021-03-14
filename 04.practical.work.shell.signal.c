799f27aa1e1ee38719cbfd0f9377238bb0911cbc
Hieu <goldenrezgun@gmail.com>
2021-03-14 11:34:55 +0700

    Practical work 5

--- 04.practical.work.shell.signal.c
+++ 04.practical.work.shell.signal.c
@@ -1,31 +1,59 @@
-#include <signal.h>
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
 
-void handler(int signal_num) {
-printf("Signal %d => ", signal_num);
-switch (signal_num) {
-case SIGTSTP:
-printf("pause\n"); break;
-case SIGINT:
-case SIGTERM:
-printf("Terminated\n");
-exit(0);
-break;
+int childpid = 0;
+
+void handler(int signal_num){
+	printf("Signal %d => ", signal_num);
+	switch (signal_num) {
+	case SIGTSTP:
+		printf("pause\n"); 
+		if (childpid) kill(childpid,SIGTSTP);
+		break;
+	case SIGINT:
+	case SIGTERM:
+		printf("Terminated\n");
+		exit(0);
+		break;
+	}
 }
-}
-int main(void) {
-// ctrl z
+
+
+int main() {
 signal(SIGTSTP, handler);
-// ctrl c or killed
 signal(SIGINT, handler);
 signal(SIGTERM, handler);
-while (1) {
-sleep(1);
-printf(".\n");
+
+while(1){
+	printf("customShell: ");
+	int pid = fork();
+	char command[256];
+	if (pid == 0) {
+
+		fgets(command,sizeof(command),stdin);
+
+                if(strcmp(command,"/q\n") == 0){
+                        return 0;
+                }
+
+		int pid = fork();
+		if(pid){
+			childpid = pid;
+			waitpid(pid,NULL,0);
+		}else{
+			char *args[]= {"/bin/bash", "-c",command,NULL};
+			execvp("/bin/bash", args);
+		}
+	}
+	else {
+		
+		wait(0);
+	}
 }
-printf("end");
-return 0;
+	return 0;
 }
